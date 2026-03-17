901e35efc8f779cf04d906d7de2fc910d55bcb8f
tmanl <nmanful@hotmail.com>
2026-03-16 20:59:29 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.shopify.com', 'natures-creamery.myshopify.com'],
+    unoptimized: true,
+    domains: [
+      'cdn.shopify.com',
+      'natures-creamery.myshopify.com',
+    ],
   },
 }
 
